99ed23a38a5d78f5330a67eb73f57dc615d5b517
sungwoo <ttlhi10@gmail.com>
2025-01-17 00:44:46 +0900

    lfs

--- ffmpeg-master-latest-win64-gpl-shared/include/libswscale/version_major.h
+++ ffmpeg-master-latest-win64-gpl-shared/include/libswscale/version_major.h
@@ -1,35 +1,3 @@
-/*
- * This file is part of FFmpeg.
- *
- * FFmpeg is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * FFmpeg is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- */
-
-#ifndef SWSCALE_VERSION_MAJOR_H
-#define SWSCALE_VERSION_MAJOR_H
-
-/**
- * @file
- * swscale version macros
- */
-
-#define LIBSWSCALE_VERSION_MAJOR   8
-
-/**
- * FF_API_* defines may be placed below to indicate public API that will be
- * dropped at a future version bump. The defines themselves are not part of
- * the public API and may change, break or disappear at any time.
- */
-
-#endif /* SWSCALE_VERSION_MAJOR_H */
+version https://git-lfs.github.com/spec/v1
+oid sha256:59377e8cf1cf5243456f800078d2d32d654552682ebfcae3c0a90b82a950fc56
+size 1175
